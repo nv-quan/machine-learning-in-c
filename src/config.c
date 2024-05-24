f4b966efd5152bafa4296270eb161725dfa56046
Quan Nguyen <quannguyenvan1701@gmail.com>
2024-05-24 23:21:21 +0900

    Update data types

--- src/config.c
+++ src/config.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "io.h"
 #include <string.h>
 
 Config get_config() {
--- src/data.h
+++ src/data.h
@@ -2,10 +2,29 @@
 #define DATA_H
 #include <stdlib.h>
 
+typedef enum data_type {
+  INT,
+  LONG,
+  FLOAT,
+  DOUBLE
+} DataType;
+
+typedef union number {
+  int _int;
+  long int _long;
+  float _float;
+  double _double;
+} Number;
+
 typedef struct data_tag {
-  double x;
-  double y;
+  DataType type;
+  Number value;
 } Data;
+
+typedef struct data_list_tag {
+  Number *content;
+  size_t length;
+}
 
 typedef struct double_array_tag {
   double *content;
@@ -17,5 +36,6 @@ typedef struct double_2d_array_tag {
   size_t width;
   size_t height;
 } Double2DArray;
+
 
 #endif
