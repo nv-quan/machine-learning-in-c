b1a342596c4db0c5a2be91fc6740c2b43f9b73e2
Quan Nguyen <quannguyenvan1701@gmail.com>
2024-06-11 23:50:19 +0900

    Try using check

--- tests/test.c
+++ tests/test.c
@@ -1,3 +1,15 @@
+#include <check.h>
+#include <custom_math.h>
+
+START_TEST(test_double_eq)
+{
+  double a = 1.23456;
+  double b = 1.23457;
+  double c = 1.234561;
+  double d = 1.234559;
+  ck_assert_int_ne(double_eq(a, b), 0);
+}
+END_TEST
 int main()
 {
   return 0;
