d93652e8e3af63316d7654dbe7ee52ac3292f864
Quan Nguyen <quannguyenvan1701@gmail.com>
2024-06-12 23:51:16 +0900

    Update ac and am files to make check

--- test/test.c
+++ test/test.c
@@ -1,3 +1,5 @@
-#include <check.h>
-
+int main()
+{
+  return 0;
+}
 
--- tests/test.c
+++ tests/test.c
@@ -1,5 +1,7 @@
 #include <check.h>
 #include <custom_math.h>
+#include <stdlib.h>
+#include "utils.h"
 
 START_TEST(test_double_eq)
 {
@@ -7,10 +9,30 @@ START_TEST(test_double_eq)
   double b = 1.23457;
   double c = 1.234561;
   double d = 1.234559;
-  ck_assert_int_ne(double_eq(a, b), 0);
+  ck_assert(double_eq(a, b));
 }
 END_TEST
-int main()
+
+Suite * math_suite(void) {
+  Suite *s;
+  TCase *tc_core;
+  s = suite_create("Math test");
+  tc_core = tcase_create("Basic");
+  tcase_add_test(tc_core, test_double_eq);
+  suite_add_tcase(s, tc_core);
+  return s;
+}
+int main(void)
 {
-  return 0;
+  int number_failed;
+  Suite *s;
+  SRunner *sr;
+
+  s = math_suite();
+  sr = srunner_create(s);
+
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
