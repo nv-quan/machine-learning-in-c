9154e39ee1cb8d55604d5673a24aa4171b7900b3
Quan Nguyen <quannguyenvan1701@gmail.com>
2024-06-16 00:30:21 +0900

    Save test log

--- tests/test.c
+++ tests/test.c
@@ -25,6 +25,7 @@ main(void) {
   srunner_add_suite(sr, math_suite());
 
   srunner_run_all(sr, CK_VERBOSE);
+  srunner_set_log(sr, "test.log");
   number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
